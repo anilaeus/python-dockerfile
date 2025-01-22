af8e4ae262de567a74887a947193b53572ac7803
Anıl DEMİR <anilaeus@gmail.com>
2025-01-22 17:09:55 +0300

    67

--- app.py
+++ app.py
@@ -1,42 +1,53 @@
+import uuid
 from flask import Flask, request
+from db import stores, items
 
 app = Flask(__name__)
 
 
-stores = [{"name": "My Store", "items": [{"name": "Chair", "price": 15.99}]}]
-
-
 @app.get("/stores")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/stores")
 def create_store():
     request_data = request.get_json()
-    print(f"name: {request_data['name']}")
-    print(f"items: {request_data['items']}")
-    stores.append(request_data)
-    return {"stores": stores}, 201
+    store_id = uuid.uuid4().hex
+    new_store = {**request_data, "id": store_id}
+    stores[store_id] = new_store
+    return new_store, 201
 
 
-@app.get("/stores/<string:name>")
-def get_store(name):
-    print(f"Request name is {name} ")
-    store = next((store for store in stores if store["name"] == name), None)
+@app.post("/item")
+def create_item():
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "Store not found"}, 404
 
-    if store is None:
-        return {"message": "Store is not found"}, 404
+    item_id = uuid.uuid4().hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item
+    return item, 201
 
-    return {"store": store}
 
+@app.get("/stores/<string:store_id>")
+def get_store(store_id):
 
-@app.get("/stores/<string:store_name>/items")
-def get_store_items(store_name):
-
-    store = next((store for store in stores if store["name"] == store_name), None)
-
-    if store is None:
+    try:
+        return {"store": stores[store_id]}
+    except KeyError:
         return {"message": "No such store exists"}, 404
 
-    return {"store_items": store["items"]}, 200
+
+@app.get("/items")
+def get_items():
+    return {"items": list(items.values())}
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return {"item": items[item_id]}
+    except KeyError:
+        return {"message": "No such item has been found"}, 404
